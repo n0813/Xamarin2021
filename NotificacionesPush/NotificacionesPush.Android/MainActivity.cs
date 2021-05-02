26e90439a1555ea98ff14d94f9d31a005d221da8
Fernando Camarillo <camarillo.itsl@gmail.com>
2021-05-01 19:33:58 -0500

    animaciones

---
+++ NotificacionesPush/NotificacionesPush.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Xamarin.Essentials;
+
+namespace NotificacionesPush.Droid
+{
+    [Activity(Label = "NotificacionesPush", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            IsPlayServicesAvailable();
+
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            bool isGooglePlayServce = resultCode != ConnectionResult.Success;
+            Preferences.Set("isGooglePlayServce", isGooglePlayServce);
+
+        }
+
+
+    }
+}
---
+++ loginPanCake/loginPanCake/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace loginPanCake
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            await Task.Delay(1000);
+
+            await panel1.FadeTo(1, 1000, Easing.SinInOut);
+
+            await Task.WhenAll(
+
+                lblTitulo.TranslateTo(0, 0, 1000, Easing.BounceOut),
+                txtUser.TranslateTo(0, 0, 1000, Easing.BounceOut),
+                txtPass.TranslateTo(0, 0, 1000, Easing.BounceOut),
+                btnIngresar.FadeTo(1, 2000, Easing.SinInOut)
+
+            );
+        }
+
+    }
+}
